82850accd772a929e21b157682373d2a123c2d9e
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-08-12 23:19:27 +0545

    Stack and Heap Memory

--- day_02/01_stack_and_heap_memory.js
+++ day_02/01_stack_and_heap_memory.js
@@ -1,1 +1,25 @@
-console.log('tttest');
+
+// Stack(Primitive), Heap Memory(Non-primitive)
+ 
+// Stack Memory Example // Here the value of the variable `myname` is not changed
+let myName = "Saggybottomboy";
+
+let anotherName = myName;
+anotherName = "Tek Raj Bhatta";
+
+console.log(myName);
+console.log(anotherName);
+
+// Heap Memory Example // Here the value of the `useOne.email` is changed
+let userOne = {
+    email: "saggy@gmail.com",
+    phone: "123456789"
+};
+
+let userTwo = userOne;
+
+userTwo.email = "tekrajbhatta@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
